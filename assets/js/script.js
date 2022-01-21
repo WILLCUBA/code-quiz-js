305880c6b0180bb9d7f5fa4ddd1193653c7f1b67
Will <wilito1210@gmail.com>
2022-01-20 22:28:34 -0500

    css done

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,8 +51,8 @@ var timer = function() {
         time--
         if (time < 0) {
             clearInterval(timer)
-            console.log("game over")
-            gameOver = true;
+            score = 0;
+            showAllDone()
             localStorage.setItem("score",score)
         }
     },1000)
